8b62f3ed9dc077560e0af7c62b09a87201cdfa20
Dave Roberts <dave.a.roberts@gmail.com>
2017-09-12 13:50:00 -0400

    Updating sample scrapes

--- sample.scrape.lisp
+++ sample.scrape.lisp
@@ -5,10 +5,11 @@ set :matched_links parselinks &all_links /a[href=somepattern]/
 
 set :scrape_data ( )
 each &matched_links :link {
+  go &link
   push &scrape_data obj {
-    :url        &link
-    :title      grabcss "h1"
-    :body       grabcss ".article-content"
+    :url   &link
+    :title grabcss "h1"
+    :body  grabcss ".article-content"
   }
 }
 
--- sample.scrape.rb
+++ sample.scrape.rb
@@ -1,15 +1,16 @@
-# Sample scrape
+# Sample scrape in Ruby
 go "https://news.example.com"
 all_links = grablinks
 matched_links = parseLinks(all_links, /a[href=somepattern]/)
+
 scrape_data = []
 matched_links.each do |link|
   go link
-  scraped_page = {
+  scrape_data.push {
     url:   link,
     title: grabcss("h1"),
     body:  grabcss(".article-content")
   }
-  scrape_data.push scraped_page
 end
+
 scrape_data.to_json
