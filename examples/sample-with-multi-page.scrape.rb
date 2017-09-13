0942daf9969eb8c3c5e810de6424cd791ff7f6ca
Dave Roberts <dave.a.roberts@gmail.com>
2017-09-13 09:31:20 -0400

    Updating syntax in examples

--- examples/sample-with-multi-page.scrape.rb
+++ examples/sample-with-multi-page.scrape.rb
@@ -1,22 +1,27 @@
 # Sample scrape with multi-page articles
 go "https://news.example.com"
 all_links = grablinks
-matched_links = parseLinks(all_links, /a[href=somepattern]/)
+matched_links = parseLinks({
+  links: all_links,
+  regex: /a[href=somepattern]/
+})
 scrape_data = []
 matched_links.each do |link|
   go link
   body_parts = []
   body_parts.push grabcss(".article-content")
-  if has_element? ".next_page"
+  page = 1
+  loop do
+    break if !has_element?(".next_page")
     click ".next_page"
+    page = page + 1
     body_parts.push grabcss(".article-content")
   end
   body = body_parts.join
-  scraped_page = {
+  scrape_data.push {
     url: link,
     title: grabcss("h1"),
     body: body
   }
-  scrape_data.push scraped_page
 end
 scrape_data.to_json
